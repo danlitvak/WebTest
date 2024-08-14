3053f8a5bf327fb1c69879b642de47a23be25859
danlitvak <daniel.litvak.64@gmail.com>
2024-08-14 04:06:54 +0000

    circle stuff

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
+let pos;
+let vel;
+let acc;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  pos = new p5.Vector(width/2, height/2);
+  vel = new p5.Vector(0, 0);
+  acc = new p5.Vector(0, 0);
 }
 
 function draw() {
   background(0);
+  noCursor();
 
-  noStroke();
-  fill(255);
-  ellipse(200, 200, 20, 20);
+  acc.add(
+    p5.Vector.sub(pos, new p5.Vector(mouseX, mouseY)).limit(1).div(-1000)
+  ).mult(0.8);
+  vel.add(acc);
+  pos.add(vel);
+
+  translate(pos);
+  stroke(255);
+  noFill();
+  circle(0, 0, 30);
+  let theta = atan2(mouseY - pos.y, mouseX - pos.x);
+  rotate(theta);
+  arc(0, 0, 40, 40, - 0.5, + 0.5);
+  resetMatrix();
+  line(pos.x, pos.y, mouseX, mouseY);
+  circle(mouseX, mouseY, 10);
 }
